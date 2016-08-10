4aa2a71c63b1eb3278a15af249b94db27c478396
Dylan Richards <dylanerichards@gmail.com>
2016-08-09 22:49:02 -0700

    Remove trailing whitespace

--- day-1/bonus_code/tax.rb
+++ day-1/bonus_code/tax.rb
@@ -1,10 +1,10 @@
-# Description: Create a program which asks the user to enter in a dollar value and the 
-# current tax percentage. 
+# Description: Create a program which asks the user to enter in a dollar value and the
+# current tax percentage.
 
-# Then print out the subtotal along with the total plus tax (grand total).
+# Then print out the subtotal along with the total plus tax (grand total)
 
 # Tips: This is a very simple program where you collect the value from the user, have the user
-# enter in a tax value, add the tax to the total and print out the result. If the value the 
+# enter in a tax value, add the tax to the total and print out the result. If the value the
 # user enters is $1.00 and the tax is 5% then the total is $1.05.
 
 # User inputs value to be deducted
